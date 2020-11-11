9ca9f40defc30af4547e038ca5023fa374646030
Shlomi Baruch <shlomibaruch5@gmail.com>
2020-11-11 14:31:04 +0200

    heroku

--- car_rental_client/src/components/Car.js
+++ car_rental_client/src/components/Car.js
@@ -51,11 +51,8 @@ import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { Button, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
--- car_rental_client/src/components/Navbar.js
+++ car_rental_client/src/components/Navbar.js
@@ -5,7 +5,6 @@ import { useHistory, Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import AuthOptions from './auth-option/AuthOption'
